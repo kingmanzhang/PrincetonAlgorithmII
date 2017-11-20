b8b1f60f2d4b1a4ea07417fcd77f61e1b5c4754b
Aaron Zhang <kingmanzhang@gmail.com>
2017-11-20 08:00:14 -0500

    work on outcast

--- src/main/java/com/kingmanzhang/ProjectI/Outcast.java
+++ src/main/java/com/kingmanzhang/ProjectI/Outcast.java
@@ -8,17 +8,34 @@ public class Outcast {
     }
 
     public String outcast(String[] nouns) {
+
         int [] d = new int[nouns.length];
-        int[][] dist = new int[][];
+        int[][] dist = new int[nouns.length][nouns.length];
         for (int i = 0; i < nouns.length; i++) {
-            dist[i][i] = 0;
+            d[i] = 0;
+            for (int j = 0; j < nouns.length; j++) {
+                dist[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < nouns.length; i++) {
             for (int j = 0; j < i; j++) {
                 dist[i][j] = dist[j][i] = this.wordnet.distance(nouns[i], nouns[j]);
             }
         }
+
+        int outcast_i = 0;
+        int outcast_dist = Integer.MIN_VALUE;
         for (int i = 0; i < nouns.length; i++) {
-            d[i] =
+            for (int j = 0; j < nouns.length; j++) {
+                d[i] += dist[i][j];
+            }
+            if (d[i] > outcast_dist) {
+                outcast_i = i;
+                outcast_dist = d[i];
+            }
         }
+        return nouns[outcast_i];
     }
 
     public static void main(String[] args){
