55a269ede1e529d523ddddf40414aeb3d118fd4c
Xingmin Zhang <kingmanzhang@gmail.com>
2017-12-03 21:17:44 -0500

    add documentation

--- src/main/java/com/kingmanzhang/ProjectIII/BaseballElimination.java
+++ src/main/java/com/kingmanzhang/ProjectIII/BaseballElimination.java
@@ -1,24 +1,33 @@
 package com.kingmanzhang.ProjectIII;
 
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-import java.util.*;
 
 public class BaseballElimination {
 
     private int numTeams;
     private LinkedHashMap<String, Integer> teamVertices; //teamVertices vertices
-    private LinkedHashMap<Integer, HashSet<Integer>> gameVertices; //game vertices TODO: may be unnecessary
+    //private LinkedHashMap<Integer, HashSet<Integer>> gameVertices; //game vertices TODO: may be unnecessary
     private int [] wins;
     private int [] losses;
     private int [] remaining;
-    private int[][] remainingGames; //gameVertices between two teamVertices TODO: is this really necessary?
+    //private int[][] remainingGames; //gameVertices between two teamVertices TODO: is this really necessary?
+    private HashMap<HashSet<Integer>, Integer> remainingGames;
     private int verticesFlownetwork; //total vertices in the flownetwork
-    //private FlowNetwork flowNetwork; //construct flowNetwork
     private FordFulkerson fordFulkerson; //perform fordFulkerson analysis of the flowNetwork
     private double MAXPOINTSREMAINING = 0.0; //points for unplayed games among non-interested teams
     private ArrayList<String> R = new ArrayList<>(); //subset of teams that eliminate a query team
-
+    private boolean[] isChecked;
 
     public BaseballElimination(String filename) { // create a baseball division from given filename in format specified below
         if (filename == null) {
@@ -31,9 +40,12 @@ public class BaseballElimination {
             this.wins = new int[numTeams];
             this.losses = new int[numTeams];
             this.remaining = new int[numTeams];
-            this.remainingGames = new int[numTeams][numTeams];
+            //this.remainingGames = new int[numTeams][numTeams];
+            this.remainingGames = new HashMap<>();
             this.teamVertices = new LinkedHashMap<>();
-            this.gameVertices = new LinkedHashMap<>();
+            //this.gameVertices = new LinkedHashMap<>();
+            this.isChecked = new boolean[numTeams];
+            Arrays.fill(isChecked, Boolean.FALSE);
         }
 
         Scanner scnr;
@@ -48,7 +60,14 @@ public class BaseballElimination {
             this.remaining[i] = scnr.nextInt();
             int j = 0;
             while(scnr.hasNextInt()) {
-                this.remainingGames[i][j++] = scnr.nextInt(); //TODO: no need to do this for every line
+                //this.remainingGames[i][j++] = scnr.nextInt(); //TODO: no need to do this for every line
+                int toPlay = scnr.nextInt();
+                HashSet<Integer> set = new HashSet<>();
+                set.add(i);
+                set.add(j++);
+                if (!remainingGames.containsKey(set)) {
+                    remainingGames.put(set, toPlay);
+                }
             }
             i++;
         }
@@ -80,16 +99,26 @@ public class BaseballElimination {
 
     }
 
+    /**
+     * Return number of teams
+     */
     public int numberOfTeams() {// number of teamVertices
 
         return this.numTeams;
     }
 
+    /**
+     * Return all teams
+     * @return
+     */
     public Iterable<String> teams() {// all teamVertices
 
         return teamVertices.keySet();
     }
 
+    /**
+     * A private method to check whether argument is a valid team.
+     */
     private void validateTeam(String team) {
 
         if (!teamVertices.containsKey(team)) {
@@ -97,70 +126,75 @@ public class BaseballElimination {
         }
 
     }
+
+    /**
+     * Return number of wins by query team
+     */
     public int wins(String team) {// number of wins for given team
 
         validateTeam(team);
         return wins[teamVertices.get(team)];
     }
 
+    /**
+     * Return number of losses by query team
+     */
     public int losses(String team) {// number of losses for given team
 
         validateTeam(team);
         return losses[teamVertices.get(team)];
     }
 
+    /**
+     * Return number of games to play by query team
+     */
     public int remaining(String team) {// number of remaining gameVertices for given team
 
         validateTeam(team);
         return remaining[teamVertices.get(team)];
     }
 
+    /**
+     * Return number of games to be played between two teams
+     */
+
     public int against(String team1, String team2) {// number of remaining gameVertices between team1 and team2
 
         validateTeam(team1);
         validateTeam(team2);
-        return this.remainingGames[this.teamVertices.get(team1)][this.teamVertices.get(team2)];
-
-        /**
-         //this will be very efficient, but maybe implement a simpler solution
-        HashSet<Integer> teamPair = new HashSet<>();
-        teamPair.add(teamVertices.get(team1));
-        teamPair.add(teamVertices.get(team2));
-        return gameVertices.get(teamPair);
-         **/
-
+        HashSet<Integer> set = new HashSet<>();
+        set.add(teamVertices.get(team1));
+        set.add(teamVertices.get(team2));
+        return this.remainingGames.get(set);
+        //return this.remainingGames[this.teamVertices.get(team1)][this.teamVertices.get(team2)];
     }
 
+    /**
+     * Test whether query team can be trivially eliminated (there exists at least one team that has more wins than query
+     * team can theorically win (by winning all unplayed games)
+     */
     private void trivialElimination(String team) {
 
         int max = wins(team) + remaining(team);
- //StdOut.println("Enter trivial Elimination test. max win for query team is " + max);
         for (String other : teams()) { //if any team already has more wins, then query team is eliminated
             if (!other.equals(team) && wins(other) > max) {
- //StdOut.println("Team " + other + " already has more wins than query team");
                 R.add(other);
             }
         }
     }
 
+    /**
+     * If a query team cannot be trivally eliminated, then use Ford-Fulkerson method to determine whether it is
+     * eliminated.
+     */
     private void nontrivialElimination(String team) {
 
-//StdOut.println("enter nontrivial test. this.R should be empty. Is it empty: " + this.R.isEmpty());
         //if there are 5 teams, the number of vertices in the flow network should be
         //5 (1 is not used) + 4 * 3 / 2 (game vertices) + 1 (source) + 1 (target)
         this.verticesFlownetwork = this.numTeams + (this.numTeams - 1) * (this.numTeams - 2) / 2 + 2;
         int v = teamVertices.get(team); //this vertice will not be used in the flow network
         //create a flow network. Last vertice is target (verticeFlownetwork - 1), second last is source (verticeFlownetwork - 2)
         FlowNetwork flowNetwork = new FlowNetwork(this.verticesFlownetwork);
-
-        /**
-        for (String other : teams()) { //edges from team vertices to sink (last of flowNetwork)
-            if (!other.equals(team)) {
-                FlowEdge flowEdge = new FlowEdge(teamVertices.get(other), verticesFlownetwork - 1, wins(team) + remaining(team) - wins(other));
-                flowNetwork.addEdge(flowEdge);
-            }
-        }
-        **/
 
         //create edges from team vertices to target
         for (int i = 0; i < numTeams; i++) {
@@ -178,11 +212,10 @@ public class BaseballElimination {
                     HashSet<Integer> pair = new HashSet<>();
                     pair.add(i);
                     pair.add(j);
-  //StdOut.printf("game pair: [%d, %d]", i, j);
-  //in case I need to see what is in game vertices
-  this.gameVertices.put(iGameVertices, pair);
-                    MAXPOINTSREMAINING += remainingGames[i][j];
-                    FlowEdge source_game_edge = new FlowEdge(verticesFlownetwork - 2, iGameVertices, remainingGames[i][j]);
+                    //MAXPOINTSREMAINING += remainingGames[i][j];
+                    MAXPOINTSREMAINING += remainingGames.get(pair);
+                    //FlowEdge source_game_edge = new FlowEdge(verticesFlownetwork - 2, iGameVertices, remainingGames[i][j]);
+                    FlowEdge source_game_edge = new FlowEdge(verticesFlownetwork - 2, iGameVertices, remainingGames.get(pair));
                     FlowEdge game_team1_edge = new FlowEdge(iGameVertices, i, Double.MAX_VALUE);
                     FlowEdge game_team2_edge = new FlowEdge(iGameVertices, j, Double.MAX_VALUE);
                     flowNetwork.addEdge(source_game_edge);
@@ -198,9 +231,11 @@ public class BaseballElimination {
                 this.R.add(other);
             }
         }
- //StdOut.println("Exiting nontrivial test. Is this.R empty now: " + this.R.isEmpty());
     }
 
+    /**
+     * Test whether a query team is mathematically eliminated.
+     */
     public boolean isEliminated(String team) { // is given team eliminated?
 
         validateTeam(team);
@@ -208,6 +243,7 @@ public class BaseballElimination {
             this.R = new ArrayList<>();
         }
         this.MAXPOINTSREMAINING = 0.0;
+        this.isChecked[teamVertices.get(team)] = true;
         //first check whether it is a trivial elimination
         trivialElimination(team);
         if (!this.R.isEmpty()) {
@@ -216,15 +252,23 @@ public class BaseballElimination {
         //if not trivially eliminated, test nontrivial elimination
         nontrivialElimination(team);
         //if max flow is the same as unplayed points, then x is not eliminated
- //StdOut.printf("Max flow: %f, MAX POINTS Remaining: %f \n", this.fordFulkerson.value(), MAXPOINTSREMAINING);
         return !(Math.abs(this.fordFulkerson.value() - MAXPOINTSREMAINING) < 0.001);
     }
 
+    /**
+     * Return subset of teams that eliminate query team
+     */
     public Iterable<String> certificateOfElimination(String team) {// subset R of teamVertices that eliminates given team; null if not eliminated
 
         validateTeam(team);
-        return this.R;
-
+        if (!isChecked[teamVertices.get(team)]) {
+            isEliminated(team);
+        }
+        if (this.R.isEmpty()) {
+            return null;
+        } else {
+            return new ArrayList<>(this.R);
+        }
     }
 
     public static void main(String[] args) {
